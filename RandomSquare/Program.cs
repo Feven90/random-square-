1e9c077bde142a9ed0fa0dd0a99d36a07d97d4ff
unknown <f.mulugeta90@gmail.com>
2019-03-11 23:19:45 -0500

    almost there

---
+++ RandomSquare/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RandomSquare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            List<int> squaredNumberz = new List<int>();
+            Random randomNumber = new Random();
+            
+
+            for (int i = 0; i < 20; i++)
+            {
+                
+               var radomNumbers =  randomNumber.Next(50);
+                numbers.Add(radomNumbers);
+               
+            }
+            foreach (var number in numbers)
+            {
+                Console.Write($"{number } {" "} ");
+            }
+            foreach (var squar in numbers)
+            {
+               int squaredNumber = squar * squar;
+                squaredNumberz.Add(squaredNumber);
+
+                Console.WriteLine(squaredNumber);
+            }
+            foreach (var odd in squaredNumberz )
+            {
+                if (odd % 2 == 0)
+                {
+                    Console.Write(odd);
+                }
+            }
+            Console.ReadLine();
+            {
+
+            }
+        }
+
+    }
+}
